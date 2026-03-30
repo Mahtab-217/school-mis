7c088369791b281ea2260ba2a4e9dc59d4e9300f
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-03-30 21:28:28 +0430

    new file of school

---
+++ app/Models/School.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class School extends Model
+{
+    //
+    protected $filable=[
+        "name",
+        "whatsapp-link",
+        "facebook-link"
+    ];
+}
